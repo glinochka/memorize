f404321b67dddd6e5823aa274508a5b7bd8e876c
glinochka <geokan33@gmail.com>
2025-03-23 00:04:40 +0300

    articles_wrapped

--- memorize/texts_storage/utils.py
+++ memorize/texts_storage/utils.py
@@ -2,20 +2,30 @@ from django.conf import settings
 from fnmatch import fnmatch
 import os
 from bs4 import BeautifulSoup
+from bs4.element import NavigableString
+
+def wrap_words_in_tag(text_node, tag_name, is_a):
+    words = text_node.split()  
+    wrapped_words = [f"<{tag_name}>{word}</{tag_name}>" for word in words] 
+
+    if is_a:
+        new_text = '&nbsp;' + " ".join(wrapped_words) + '&nbsp;'
+    else:
+        new_text = " ".join(wrapped_words) 
+
+    text_node.replace_with(BeautifulSoup(new_text, 'html.parser'))
 
 
 
-# Функция для оборачивания слов в тег
+def wrap_w(soup):
+    for i in soup.body.descendants:
+        if type(i) == NavigableString and not i.isspace():
+            wrap_words_in_tag(i, 'w',  any('a' == p.name for p in i.parents))
 
-def wrap_words_in_tag(element, tag_name):
+    return str(soup)
 
-    for text_node in element.find_all(string=True):# Находим все текстовые узлы
-        if text_node.strip():  # Игнорируем пустые текстовые узлы
-                
-                words = text_node.split()  # Разделяем текст на слова
-                wrapped_words = [f"<{tag_name}>{word}</{tag_name}>" for word in words]  # Обёртываем каждое слово
-                new_text = " ".join(wrapped_words)  # Соединяем слова обратно
-                text_node.replace_with(BeautifulSoup(new_text, 'html.parser'))  # Заменяем исходный текст
+
+
 
 def savy_html(uploaded_file):
     with open(os.path.join(settings.MEDIA_ROOT, uploaded_file),'r', encoding='utf-8') as file:
@@ -23,29 +33,17 @@ def savy_html(uploaded_file):
 
     soup = BeautifulSoup(data, 'html.parser')
     
-        
-    # Применяем функцию ко всем элементам body
-    chunk = len(soup.body.find_all())//100
-    print(len(soup.body.find_all()))
-    i = 0
-    for element in soup.body.find_all():
-        i+=1
-        print(i)
-        for c, ch in enumerate(element.contents):
-            st = element.contents[c].string
-            if ch == st and ch != ' ':
-                wrap_words_in_tag(element, 'w  class= "hover-text-warning"')
-
     for a_tag in soup.find_all('a'):
         if a_tag.get('target') == '_self': continue
         if a_tag.get('href'):
             if a_tag.get('href')[0] == '#': continue
         a_tag['target'] = '_blank'
 
-    
-    data = str(soup)
+    data = wrap_w(soup)
+
     if not fnmatch(data, '{% verbatim %}*{% endverbatim %}'):
         data = '{% verbatim %}' + data + '{% endverbatim %}'
+
     with open(os.path.join(settings.MEDIA_ROOT, uploaded_file),'w', encoding='utf-8') as file:
         file.write(data)
     
--- test.py
+++ test.py
@@ -1,33 +1,30 @@
 from bs4 import BeautifulSoup
+from bs4.element import NavigableString
 
-with open('Dockerfile_reference___Docker_Docs.html','r', encoding='utf-8') as f:
+with open('os - Miscellaneous operating system interfaces - Python 3.13.2 documentation.html','r', encoding='utf-8') as f:
     html_content = f.read()
 
-# Создание объекта BeautifulSoup
 soup = BeautifulSoup(html_content, 'html.parser')
 
-# Функция для оборачивания слов в тег
-def wrap_words_in_tag(element, tag_name):
+def wrap_w(soup):
+            
+    def wrap_words_in_tag(text_node, tag_name):
+        words = text_node.split()  
+        wrapped_words = [f"<{tag_name}>{word}</{tag_name}>" for word in words] 
+        new_text = " ".join(wrapped_words) 
+        text_node.replace_with(BeautifulSoup(new_text, 'html.parser'))
 
-    for text_node in element.find_all(string=True):# Находим все текстовые узлы
-        if text_node.strip():  # Игнорируем пустые текстовые узлы
-                
-                words = text_node.split()  # Разделяем текст на слова
-                wrapped_words = [f"<{tag_name}>{word}</{tag_name}>" for word in words]  # Обёртываем каждое слово
-                new_text = " ".join(wrapped_words)  # Соединяем слова обратно
-                text_node.replace_with(BeautifulSoup(new_text, 'html.parser'))  # Заменяем исходный текст
-        
-# Применяем функцию ко всем элементам body
-for element in soup.body.find_all():
-    for c, ch in enumerate(element.contents):
-        
-        st = element.contents[c].string
-        if ch == st and ch != ' ':
-            wrap_words_in_tag(element, 'w  class= "hover-text-warning"')
 
-with open('Dockerfile_reference___Docker_Docs.html','w', encoding='utf-8') as f:
+    for i in soup.body.descendants:
+        if type(i) == NavigableString and not i.isspace():
+            wrap_words_in_tag(i, 'w')
+
+    return str(soup)
+
+wrap_w(soup)
+
+with open('os - Miscellaneous operating system interfaces - Python 3.13.2 documentation.html','w', encoding='utf-8') as f:
     f.write(str(soup))
 
-    
 
 
